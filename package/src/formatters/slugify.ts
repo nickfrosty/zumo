59830763aa1cc5986618a2c1ea8d684b713e7379
nickfrosty <nfrostbutter@gmail.com>
2023-05-09 03:52:45 -0400

    feat: added slugify

---
+++ package/src/formatters/slugify.ts
@@ -0,0 +1,14 @@
+/**
+ * Slugify a string into a valid url format
+ */
+export function slugify(text: string) {
+  return text
+    .toString()
+    .normalize("NFD")
+    .replace(/[\u0300-\u036f]/g, "")
+    .toLowerCase()
+    .trim()
+    .replace(/\s+/g, "-")
+    .replace(/[^\w-]+/g, "")
+    .replace(/--+/g, "-");
+}
